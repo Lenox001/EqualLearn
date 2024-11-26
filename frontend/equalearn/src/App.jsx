bd89d2aa3f29e6f3bd2683a80700061ad0597905
Larry Lenox <larrylenx9@gmail.com>
2024-11-26 11:06:56 +0300

    first commit

---
+++ frontend/equalearn/src/App.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="container my-5">
+      <h2 className="text-info text-center">Welcome to EquaLearn</h2>
+      <p className="lead text-center">
+        The platform that empowers your learning journey.
+      </p>
+    </div>
+  );
+}
+
+export default App;
