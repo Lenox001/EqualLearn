c86837350866c101495d69b618b34ff18879140a
Larry Lenox <larrylenx9@gmail.com>
2024-11-26 14:39:39 +0300

    Added Navbar with Styling

--- frontend/equalearn/src/App.jsx
+++ frontend/equalearn/src/App.jsx
@@ -1,13 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="container my-5">
-      <h2 className="text-info text-center">Welcome to EquaLearn</h2>
-      <p className="lead text-center">
-        The platform that empowers your learning journey.
-      </p>
-    </div>
+    < >
+    <Navbar/>
+    </>
   );
 }
 
---
+++ frontend/equalearn/src/components/Navbar.jsx
@@ -0,0 +1,94 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faInfoCircle,
+  faBook,
+  faEnvelope,
+  faUserCircle,
+  faQuestionCircle,
+  faSignOutAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import "../styles/Navbar.css"; 
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand animated-brand" href="/">
+          EquaLearn
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                <FontAwesomeIcon icon={faHome} /> Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/courses">
+                <FontAwesomeIcon icon={faBook} /> Courses
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">
+                <FontAwesomeIcon icon={faInfoCircle} /> About
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="accountDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <FontAwesomeIcon icon={faUserCircle} /> Account
+              </a>
+              <ul
+                className="dropdown-menu dropdown-menu-end"
+                aria-labelledby="accountDropdown"
+              >
+                <li>
+                  <a className="dropdown-item" href="/profile">
+                    Profile
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/faqs">
+                    FAQs <FontAwesomeIcon icon={faQuestionCircle} />
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/logout">
+                    Logout <FontAwesomeIcon icon={faSignOutAlt} />
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">
+                <FontAwesomeIcon icon={faEnvelope} /> Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
